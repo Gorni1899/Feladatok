6476300113e54b1d192ec80ccf77b0f573e7ff8c
Gorni1899 <humanpyx@protonmail.com>
2023-02-16 18:00:32 +0100

    fájlok feltöltése

--- Latin-Tancok/latin-tancok.py
+++ Latin-Tancok/latin-tancok.py
@@ -1,1 +1,19 @@
 print("1.Feladat")
+"""
+Egy nagy listában fogom tárolni az adatokat aminek az egyes táncra vonatkozó elemei szótárak lesznek.
+"""
+adatok = []
+tancok = []
+tanc = {}
+
+with open("tancrend.txt", "r", encoding="utf-8") as file:
+    for sor in file:
+        if len(sor) == 3:
+            adatok.append(sor.strip())
+            tanc["nev"] = adatok[0]
+            tanc["lany"] = adatok[1]
+            tanc["fiu"] = adatok[2]
+            tancok.append(tanc)
+            adatok = []
+            szotar = {}
+print(tancok)
