d990ee69f95a0d96f33725c8189769a34c668b78
gorni <humanpyx@protonmail.com>
2023-02-17 08:50:37 +0100

    3.feladat

--- Latin-Tancok/latin-tancok.py
+++ Latin-Tancok/latin-tancok.py
@@ -3,8 +3,8 @@ print("1.Feladat")
 Egy nagy listában fogom tárolni az adatokat aminek az egyes táncra vonatkozó elemei szótárak lesznek.
 """
 adatok = []
-tancok = []
 tanc = {}
+tancok = []
 
 with open("tancrend.txt", "r", encoding="utf-8") as file:
     for sor in file:
@@ -15,10 +15,10 @@ with open("tancrend.txt", "r", encoding="utf-8") as file:
             tanc["fiu"] = adatok[2]
             tancok.append(tanc)
             adatok = []
-            szotar = {}
+            tanc = {}
 
-# teszt kiíratás
 print(tancok)
+
 
 print("\n2.Feladat")
 """
@@ -27,3 +27,15 @@ Két print utasítást fogok használni és f sztringet
 #2. Írassa ki a képernyőre, hogy melyik volt az elsőként és melyik az utolsóként bemutatott tánc neve!
 print(f"Az elsőként bemutatott tánc neve a {tancok[0]['nev']} volt.")
 print(f"Az utolsóként bemutatott tánc neve a {tancok[-1]['nev']} volt.")
+
+print("\n3.Feladat")
+"""
+Megszámlálós algoritmust használok.
+"""
+# 3. Hány pár mutatta be a sambát? A választ jelenítse meg a képernyőn!
+
+szamlalo = 0
+for tanc in tancok:
+    if tanc["nev"] == "samba":
+        szamlalo += 1
+print("A szambát "+str(szamlalo)+" pár mutatta be.")
