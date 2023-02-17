edff5e44996bae74b107de59eda6770eb5763048
gorni <humanpyx@protonmail.com>
2023-02-17 08:32:43 +0100

    2.feladat

--- Latin-Tancok/latin-tancok.py
+++ Latin-Tancok/latin-tancok.py
@@ -8,12 +8,22 @@ tanc = {}
 
 with open("tancrend.txt", "r", encoding="utf-8") as file:
     for sor in file:
-        if len(sor) == 3:
-            adatok.append(sor.strip())
+        adatok.append(sor.strip())
+        if len(adatok) == 3:
             tanc["nev"] = adatok[0]
             tanc["lany"] = adatok[1]
             tanc["fiu"] = adatok[2]
             tancok.append(tanc)
             adatok = []
             szotar = {}
+
+# teszt kiíratás
 print(tancok)
+
+print("\n2.Feladat")
+"""
+Két print utasítást fogok használni és f sztringet
+"""
+#2. Írassa ki a képernyőre, hogy melyik volt az elsőként és melyik az utolsóként bemutatott tánc neve!
+print(f"Az elsőként bemutatott tánc neve a {tancok[0]['nev']} volt.")
+print(f"Az utolsóként bemutatott tánc neve a {tancok[-1]['nev']} volt.")
