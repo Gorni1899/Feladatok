99943843f0602c4d614acc6018bfd080163c837e
humanpyx <wimble@wimble.hu>
2023-06-13 12:52:53 +0200

    Optimalizáltam a kódot

--- Benzinkut/Benzinkut.py
+++ Benzinkut/Benzinkut.py
@@ -16,12 +16,15 @@ print("1.Feladat")
 1. A mintának megfelelően olvassa be és rögzítse az üzemanyag-eladásokat! Az
 adatbekérés mindaddig folytatódjon, amíg a felhasználó csupán ENTER-t nem üt!
 """
-
+diesel_tarolas = []
 tarolt_tankolasok = []
 tankolas = input("Adja meg az üzemanyag típusát és mennyiségét! (pl. B4 vagy D23) ")
 
 while tankolas != "":
-        tarolt_tankolasok.append(tankolas)
+        if tankolas[0] == "D" and tankolas[1:] != "0":
+                diesel_tarolas.append(int(tankolas[1:]))
+        if tankolas[1:] != "0":
+                tarolt_tankolasok.append(int(tankolas[1:]))
         tankolas = input("Adja meg az üzemanyag típusát és mennyiségét! (pl. B4 vagy D23) ")
 
 print("\n2.Feladat")
@@ -30,28 +33,21 @@ print(f"A benzinkúton {len(tarolt_tankolasok)} alkalommal vásároltak.")
 
 print("\n3.Feladat")
 # 3. Adja meg, hogy hány liter benzint vásároltak összesen!
-ossz_tankolas = 0
 
-for tankolas in tarolt_tankolasok:
-        ossz_tankolas += int(tankolas[1:])
-print(f'{ossz_tankolas}l benzint adtak el összesen.')
+print(f"{sum(tarolt_tankolasok)}l benzint adtak el összesen.")
 
 print("\n4.Feladat")
 # Írja ki az egy alkalommal vásárolt átlagos
 # üzemanyag-mennyiséget két tizedesjegyre kerekítve!
 
-print(f'Egy alkalommal átlagosan {ossz_tankolas / len(tarolt_tankolasok)}l üzemanyagot vásároltak.')
+print(f'Egy alkalommal átlagosan {sum(tarolt_tankolasok) / len(tarolt_tankolasok):.2f}l üzemanyagot vásároltak.')
 
 print('\n5.Feladat')
 # Adja meg, hogy mennyi volt az egy alkalommal vásárolt legnagyobb dieselmennyiség!
 max_diesel = 0
-diesel = []
-for tankolas in tarolt_tankolasok:
-        if tankolas[0] == 'D':
-                diesel.append(int(tankolas[1:]))
 
-for maximum in diesel:
-        if max_diesel < maximum:
-                max_diesel = maximum
+for tankolas in diesel_tarolas:
+        if max_diesel < tankolas:
+                max_diesel = tankolas
 
 print(f'Az egy alkalommal eladott legnagyobb diesel mennyiség {max_diesel}l volt.')
